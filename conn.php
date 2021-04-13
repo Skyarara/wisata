203d34373a6ec341f76a0b7f6112816ec585f240
Richard Tandy Japutra <richardtandy2015@gmail.com>
2021-04-13 22:22:53 +0800

    landing page sedikit

--- conn.php
+++ conn.php
@@ -1,11 +1,8 @@
 <?php 
 
-
     $server = "localhost";
     $username = "root";
     $db_name = "wisata";
     $pass = "";
 
     $conn = mysqli_connect($server, $username, "", $db_name);
-
-?>
---
+++ latihan/tes.php
@@ -0,0 +1,17 @@
+<div class="sketchfab-embed-wrapper">
+    <iframe title="Brahma photogrammetry - materials remake" frameborder="0" allowfullscreen mozallowfullscreen="true"
+        webkitallowfullscreen="true" allow="fullscreen; autoplay; vr" xr-spatial-tracking
+        execution-while-out-of-viewport execution-while-not-rendered web-share
+        src="https://sketchfab.com/models/2b132117c0ef4e1e93b23d0185df6d15/embed">
+    </iframe>
+    <!-- <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;">
+        <a href="https://sketchfab.com/3d-models/brahma-photogrammetry-materials-remake-2b132117c0ef4e1e93b23d0185df6d15?utm_medium=embed&utm_campaign=share-popup&utm_content=2b132117c0ef4e1e93b23d0185df6d15"
+            target="_blank" style="font-weight: bold; color: #1CAAD9;">Brahma photogrammetry - materials remake</a>
+        by <a
+            href="https://sketchfab.com/Flatland?utm_medium=embed&utm_campaign=share-popup&utm_content=2b132117c0ef4e1e93b23d0185df6d15"
+            target="_blank" style="font-weight: bold; color: #1CAAD9;">Flatland</a>
+        on <a
+            href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=2b132117c0ef4e1e93b23d0185df6d15"
+            target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a>
+    </p> -->
+</div>
