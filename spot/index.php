1c6a1f4bb72acad2ff625784077b608914d766c3
Richard Tandy Japutra <richardtandy2015@gmail.com>
2021-06-09 23:26:31 +0800

    audio

--- spot/index.php
+++ spot/index.php
@@ -4,7 +4,15 @@
     include "../layout/user/navbar.php";
 
 ?>
+<!-- audio -->
+<audio id="audio_klenteng" src="audio/a.mp3"></audio>
+<audio id="audio_brahma" src="audio/b.mp3"></audio>
+<audio id="audio_pagoda" src="audio/pagoda.m4a"></audio>
+<!-- <audio id="audio_atlantis" src="audio/pagoda.m4a"></audio> -->
+<input type="text" value="0" id="playing">
+
 <div class="container-fluid">
+
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Spots</h1>
@@ -30,7 +38,7 @@
                     <button type="button" data-opt="2" class="btn btn-info trigger">
                         Camera 360
                     </button>
-                    <button type="button" class="btn btn-primary">
+                    <button type="button" class="btn btn-primary audio" data-opt="1">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                             class="bi bi-volume-up" viewBox="0 0 16 16">
                             <path
@@ -63,7 +71,7 @@
                     <button type="button" data-opt="4" class="btn btn-info trigger">
                         Camera 360
                     </button>
-                    <button type="button" class="btn btn-primary">
+                    <button type="button" class="btn btn-primary audio" data-opt="2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                             class="bi bi-volume-up" viewBox="0 0 16 16">
                             <path
@@ -99,7 +107,7 @@
                     <button type="button" data-opt="6" class="btn btn-info trigger">
                         Camera 360
                     </button>
-                    <button type="button" class="btn btn-primary">
+                    <button type="button" class="btn btn-primary audio" data-opt="3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                             class="bi bi-volume-up" viewBox="0 0 16 16">
                             <path
@@ -179,65 +187,4 @@
 <?php 
     include "../layout/user/footer.php";
 ?>
-
-<script>
-    $('.trigger').on('click', function () {
-        var opt = $(this).data('opt');
-        switch (opt) {
-            case 1:
-                var
-                    src =
-                    "https://www.google.com/maps/embed?pb=!4v1621882190368!6m8!1m7!1sAElPdRWzYNR96hMooqE1qw!2m2!1d-7.248555416791929!2d112.8022513157001!3f350.4809!4f0!5f0.7820865974627469";
-                var title = "Street View Klenteng Sanggar Agung";
-                break;
-            case 2:
-                var src =
-                    "https://www.google.com/maps/embed?pb=!4v1617530101770!6m8!1m7!1sCAoSLEFGMVFpcE1ha2pDQ0lOMXdPc2pNRF9QaUcydjJqWVBnT19kbnA4empmTWxB!2m2!1d-7.247154199999999!2d112.8025535!3f269.67448007737346!4f-1.8420999644238236!5f0.4000000000000002"
-                var title = "Camera 360 Klenteng Sanggar Agung";
-                break;
-            case 3:
-                var src =
-                    "https://www.google.com/maps/embed?pb=!4v1621882491956!6m8!1m7!1s037fb23iHDkndhUM1n8Q4Q!2m2!1d-7.246826669141439!2d112.8011770220298!3f190.27298!4f0!5f0.7820865974627469";
-                var title = "Street View Patung Brahma";
-                break;
-            case 4:
-                var src =
-                    "https://www.google.com/maps/embed?pb=!4v1617529740461!6m8!1m7!1sCAoSLEFGMVFpcE1Kc2MxaHJlVXpxeERnNUVXd0NMbUpTdUlQQm5wQjFOWkJRZ3pB!2m2!1d-7.234584225213776!2d112.7380507110095!3f349.5917912610072!4f-3.760105986796205!5f0.7820865974627469"
-                var title = "Camera 360 Patung Brahma";
-                break;
-            case 5:
-                var src =
-                    "https://www.google.com/maps/embed?pb=!4v1621882603229!6m8!1m7!1s9cKgDm9AMOeHpBubdJPsoA!2m2!1d-7.254165761592636!2d112.8052025548826!3f5.217394177003184!4f3.521589638456902!5f0.7820865974627469"
-                var title = "Street View Pagoda Tian Ti";
-                break;
-            case 6:
-                var src =
-                    "https://www.google.com/maps/embed?pb=!4v1617679427366!6m8!1m7!1sCAoSLEFGMVFpcE43TjhmVEZxN3Q4UTdxSjRVVUlaVjdnd0NmVEg1eTdyYUV0bWJS!2m2!1d-7.252997828904998!2d112.8055772811914!3f193.43792473088658!4f4.694726967575804!5f0.7820865974627469";
-                var title = "Camera 360 Pagoda Tian Ti";
-                break;
-            case 7:
-                var src =
-                    "https://www.google.com/maps/embed?pb=!4v1621882660108!6m8!1m7!1s8nc2nwespuRVLwbtnoKCdw!2m2!1d-7.253329690095828!2d112.8029222769374!3f305.43738!4f0!5f0.7820865974627469"
-                var title = "Street View Atlantis Land";
-                break;
-            case 8:
-                var src =
-                    "https://www.google.com/maps/embed?pb=!4v1617529960140!6m8!1m7!1sCAoSLEFGMVFpcE8xTmF1d3FOcHdXZC0yMkNheUdRVnFKMkxMUHdHUHZNR0xsOXp5!2m2!1d-7.254288!2d112.8018528!3f301.6142454004159!4f0.9217158648408343!5f0.7820865974627469"
-                var title = "Camera 360 Atlantis Land";
-                break;
-            default:
-                console.log("error");
-                alert("Terjadi Kesalahan Harap Coba Lagi");
-                break;
-        }
-        $('#modal').modal('show').on('shown.bs.modal', function () {
-            $("#frame").attr("src", src);
-            $("#title").text(title);
-        });
-    });
-
-    $("#modal").on('hide.bs.modal', function () {
-        $("#frame").attr("src", "");
-        $("#title").text("");
-    });
-</script>
+<script src="spot.js"></script>
